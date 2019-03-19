6a8a56775f98d12890770d6b8f5891097846954c
Vadim Iskuchekov <root@egregors.com>
2019-03-19 21:00:16 +0100

    let

--- f.hs
+++ f.hs
@@ -88,3 +88,12 @@ calcBmis xs = [bmi w h | (w, h) <- xs]
   where
     bmi weight height = weight / height ^ 2
 
+-- cylinder surface
+-- let <bindings> in <expression>.
+-- [let square x = x * x in (square 5, square 3, square 2)]  
+-- (let a = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)  
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+   in sideArea + 2 * topArea
