356660dd98e1b4f563f4b54f1e087c1ed9afc9e5
Vadim Iskuchekov <root@egregors.com>
2019-04-05 20:47:10 +0200

    take reverse repeat

--- rec.hs
+++ rec.hs
@@ -9,7 +9,24 @@ maximum' (x:xs)
 
 -- maximum with max
 maximum'' :: (Ord a) => [a] -> a
-maximum'' [] = error "empty"
-maximum'' [x] = x
+maximum'' []     = error "empty"
+maximum'' [x]    = x
 maximum'' (x:xs) = max x (maximum'' xs)
 
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n - 1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' []     = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
